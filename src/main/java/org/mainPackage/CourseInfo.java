c0a8c7b874a4cd5b37b2bdee49b3073017e7d0df
Gabriel Scofield <142074070+GQSco@users.noreply.github.com>
2024-10-13 22:21:55 -0700

    Update CourseInfo.java

--- src/main/java/org/mainPackage/CourseInfo.java
+++ src/main/java/org/mainPackage/CourseInfo.java
@@ -150,7 +150,7 @@ public class CourseInfo {
             }else if (0 < i && i < 4) {
                 if (section.isEmpty() && info[i].matches("\\w{1,2}")) { // Section
                     section = info[i];
-                } else if (info[i].contains("QZ") || info[i].contains("Laboratory")) { // TODO make sure this works, laboratories are slightly different but still act as quizzes. check EE 201
+                } else if (info[i].contains("QZ")) { // TODO make sure this works, laboratories are slightly different but still act as quizzes. check EE 201
                     type = "Quiz"; // TODO it could be a laboratory too
                 }
             } else {
